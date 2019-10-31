dc6771930ac96b7671da9f801eb2334cc7a49e80
Serg <krus-83@mail.ru>
2019-10-31 16:19:07 +0700

    creation of the first section

--- src/app/common/modules/conclusion/conclusion.component.ts
+++ src/app/common/modules/conclusion/conclusion.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ConclusionComponent implements OnInit {
   links = [
     {name: 'Моя работа', path: 'work', icon: 'work_outline'},
-    {name: 'Все разделы', path: 'general', icon: 'view_headline'},
+    {name: 'Все разделы', path: 'general', icon: 'description'},
     {name: 'Печатная форма', path: 'print-form', icon: 'print'}
     ];
   activeLink = this.links[0];
